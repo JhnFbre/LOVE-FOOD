7d873bdaee01d39f61451769ba08af4b6f7e64cd
JhnFbre <walter96_@hotmail.com>
2018-07-06 16:19:20 -0500

    Add all

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>LOVE FOOD</title>
+    <link rel="stylesheet" href="css/estilosIndex.css">
+    <link rel="icon" href="images/logoPesta%C3%B1a.png">
+</head>
+<body>
+    <div id="iz">
+        <div class="form-login">
+            <form action="php/login.php" method="post">
+                <h4>Inicio de Sesión</h4>
+                <input type="text" name="user" placeholder="Usuario">
+                <input type="text" name="contra" placeholder="Contraseña">
+                <input type="submit" value="LOGIN">
+            </form>
+        </div>
+    </div>
+    <div id="de">
+        <img src="images/redLF.png" alt="LOVEFOOD" id="img-index">
+    </div>
+</body>
+</html>
---
+++ php/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h4>Hola</h4>
+    </div>
+</body>
+</html>
