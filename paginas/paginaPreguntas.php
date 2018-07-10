913a06249fc3299209391506b14d5505d1d5d2af
JhnFbre <walterfa@outlook.es>
2018-07-10 01:05:22 -0500

    Add all questions

--- paginas/paginaPreguntas.php
+++ paginas/paginaPreguntas.php
@@ -32,53 +32,82 @@ session_start();
         <h2>Preguntas</h2>
         <form action="">
             <h4>Que prefieres comer:</h4>
-            <input type="radio" name="seafood" value="ceviche">Ceviche<br>
-            <input type="radio" name="seafood" value="encebollado">Encebollado<br>
+            <input type="radio" name="preferencia" value="hombre">Hombre<br>
+            <input type="radio" name="preferencia" value="mujer">Mujer<br>
 
-            <h4>Que prefieres comer:</h4>
-            <input type="radio" name="vegeocarne" value="ensalada">Ensalada<br>
-            <input type="radio" name="vegeocarne" value="burguer">Hamburguesa<br>
+            <h4>Desayuno Salado</h4>
+            <input type="radio" name="desayunosalado" value="bollo">Bollo<br>
+            <input type="radio" name="desayunosalado" value="bolon">Bolón<br>
+            <input type="radio" name="desayunosalado" value="empanada">Empanada<br>
+            <input type="radio" name="desayunosalado" value="patacones">Patacones<br>
+            <input type="radio" name="desayunosalado" value="tigrillo">Tigrillo<br>
             
-            <h4>Que prefieres comer:</h4>
-            <input type="radio" name="tortaoempa" value="torta">Torta<br>
-            <input type="radio" name="tortaoempa" value="empanada">Empanada<br>
+            <h4>Desayuno Dulce</h4>
+            <input type="radio" name="desayunodulce" value="waffles">Waffles<br>
+            <input type="radio" name="desayunodulce" value="panqueques">Panqueques<br>
+            <input type="radio" name="desayunodulce" value="cereal">Cereal<br>
+            <input type="radio" name="desayunodulce" value="galletas">Galletas<br>
             
-            <h4>Que prefieres comer:</h4>
-            <input type="radio" name="seafood" value="ceviche">Ceviche<br>
-            <input type="radio" name="seafood" value="encebollado">Encebollado<br>
-
-            <h4>Que prefieres comer:</h4>
-            <input type="radio" name="vegeocarne" value="ensalada">Ensalada<br>
-            <input type="radio" name="vegeocarne" value="burguer">Hamburguesa<br>
+            <h4>Almuerzo</h4>
+            <input type="radio" name="almuerzo" value="yapingacho">Yapingacho<br>
+            <input type="radio" name="almuerzo" value="pescadomariscos">Pescado y mariscos<br>
+            <input type="radio" name="almuerzo" value="secopollo">Seco de Pollo<br>
+            <input type="radio" name="almuerzo" value="arrozmenestracarne">Arroz con menestra y carne<br>
+            <input type="radio" name="almuerzo" value="tallarines">Tallarines<br>
+            <input type="radio" name="almuerzo" value="arrozpurecarne">Arroz con pure y carne<br>
             
-            <h4>Que prefieres comer:</h4>
-            <input type="radio" name="tortaoempa" value="torta">Torta<br>
-            <input type="radio" name="tortaoempa" value="empanada">Empanada<br>
+            <h4>Cena</h4>
+            <input type="radio" name="cena" value="carne">Carnes<br>
+            <input type="radio" name="cena" value="marisco">Mariscos<br>
+            <input type="radio" name="cena" value="ensalada">Ensaladas<br>
+            <input type="radio" name="cena" value="tegalleta">Té con galletas<br>
             
-            <h4>Que prefieres comer:</h4>
-            <input type="radio" name="seafood" value="ceviche">Ceviche<br>
-            <input type="radio" name="seafood" value="encebollado">Encebollado<br>
-
-            <h4>Que prefieres comer:</h4>
-            <input type="radio" name="vegeocarne" value="ensalada">Ensalada<br>
-            <input type="radio" name="vegeocarne" value="burguer">Hamburguesa<br>
+            <h4>Comidas Tipicas</h4>
+            <input type="radio" name="comidatipica" value="encebollado">Encebollado<br>
+            <input type="radio" name="comidatipica" value="guatita">Guatita<br>
+            <input type="radio" name="comidatipica" value="fritada">Fritada<br>
+            <input type="radio" name="comidatipica" value="churrasco">Churrasco<br>
+            <input type="radio" name="comidatipica" value="yaguarlocro">Yaguarlocro<br>
             
-            <h4>Que prefieres comer:</h4>
-            <input type="radio" name="tortaoempa" value="torta">Torta<br>
-            <input type="radio" name="tortaoempa" value="empanada">Empanada<br>
+            <h4>Carnes</h4>
+            <input type="radio" name="carnes" value="carnes">Carnes<br>
+            <input type="radio" name="carnes" value="pollo">Pollo<br>
+            <input type="radio" name="carnes" value="chuleta">Chuletas<br>
+            <input type="radio" name="carnes" value="costilla">Costillas<br>
+            <input type="radio" name="carnes" value="embutido">Embutidos<br>
             
-            <h4>Que prefieres comer:</h4>
-            <input type="radio" name="seafood" value="ceviche">Ceviche<br>
-            <input type="radio" name="seafood" value="encebollado">Encebollado<br>
-
-            <h4>Que prefieres comer:</h4>
-            <input type="radio" name="vegeocarne" value="ensalada">Ensalada<br>
-            <input type="radio" name="vegeocarne" value="burguer">Hamburguesa<br>
+            <h4>Postres</h4>
+            <input type="radio" name="postres" value="tiramisu">Tiramisú<br>
+            <input type="radio" name="postres" value="helado">Helado<br>
+            <input type="radio" name="postres" value="flan">Flan<br>
+            <input type="radio" name="postres" value="tortachocolate">Torta de Chocolate<br>
+            <input type="radio" name="postres" value="cheesecake">Cheesecake<br>
+            <input type="radio" name="postres" value="brownie">Brownies<br>
             
-            <h4>Que prefieres comer:</h4>
-            <input type="radio" name="tortaoempa" value="torta">Torta<br>
-            <input type="radio" name="tortaoempa" value="empanada">Empanada<br>
-            <button>Enviar</button>
+            <h4>Sabores Dulces</h4>
+            <input type="radio" name="saboresdulces" value="chocolate">Chocolate<br>
+            <input type="radio" name="saboresdulces" value="vainilla">Vainilla<br>
+            <input type="radio" name="saboresdulces" value="fresa">Fresas<br>
+            <input type="radio" name="saboresdulces" value="mora">Mora<br>
+            <input type="radio" name="saboresdulces" value="rompasas">Rompasas<br>
+            <input type="radio" name="saboresdulces" value="Chicle">Chicle<br>
+            
+            <h4>Bebidas</h4>
+            <input type="radio" name="bebida" value="cafe">Café<br>
+            <input type="radio" name="bebida" value="jugonatural">Jugo Natural<br>
+            <input type="radio" name="bebida" value="te">Té<br>
+            <input type="radio" name="bebida" value="vino">Vino<br>
+            <input type="radio" name="bebida" value="cocacola">Coca Cola<br>
+            <input type="radio" name="bebida" value="agua">Agua<br>
+            <input type="radio" name="bebida" value="cerveza">Cerveza<br>
+            
+            <h4>Comidas Extranjeras</h4>
+            <input type="radio" name="comidaextranjera" value="comidachina">Comida China<br>
+            <input type="radio" name="comidaextranjera" value="comidaitaliana">Comida Italiana<br>
+            <input type="radio" name="comidaextranjera" value="comidamexicana">Comida Mexicana<br>
+            <input type="radio" name="comidaextranjera" value="comidajaponesa">Comida Japonesa<br>
+            
+            <input type="submit" value="Enviar">
         </form>
     </div>
 </body>
