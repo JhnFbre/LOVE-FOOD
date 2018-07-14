270a948a6028a697cd6b478d2a8d1bdda36ab3c5
yandersantana <ysantanatavares.1996@hotmail.com>
2018-07-13 23:10:19 -0500

    My first commit

--- paginas/modelo/collector.php
+++ paginas/modelo/collector.php
@@ -2,10 +2,10 @@
 include_once('database.php');
 
 // Define configuration
-define("DB_HOST", "localhost");
-define("DB_USER", "postgres");
-define("DB_PASS", "postgres");
-define("DB_NAME", "lovefood");
+define("DB_HOST", "ec2-107-21-95-70.compute-1.amazonaws.com");
+define("DB_USER", "tziirijesvnwbd");
+define("DB_PASS", "2dd110e438d672d854e96fd24cc6f398bdeaff25707fa16e057bd4c8ed1233b2");
+define("DB_NAME", "d6nas1lincjd3e");
 
 
 class Collector extends dataBase
